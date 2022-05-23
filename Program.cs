d210a5d3b509440bee2477454e4898e3c7feae77
k-sarito <coryclark95@gmail.com>
2022-05-23 13:21:51 -0500

    Big Bang

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace GuessingGame
+{
+    class Program
+    {
+        void Main()
+        {
+            int SecretNum = 42;
+
+            Console.Write("Welcome! Guess a number!");
+            string response = Console.ReadLine();
+            int ParsedResponse = int.Parse(response);
+            Console.WriteLine(ParsedResponse);
+            
+        }
+    }
+}
+
